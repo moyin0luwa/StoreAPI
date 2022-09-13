505ed05b5329e1bab838ef27cb0b526136c45fe2
Moyinoluwalogo <m.oladipsgmail.com>
2022-09-13 18:45:32 +0100

    Set up server along with error handling middleware tested wuth a simple get route, created enviroment variables,and started up the server

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log('server starting')
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+// Error Handling
+const notFoundMiddleware = require("./src/middleware/not-found");
+const errorMiddleware = require("./src/middleware/error-handler");
+
+//express json middleware
+app.use(express.json());
+
+// Route
+app.get("/", (req, res) => {
+	res.send("STORE IS OPEN");
+});
+
+// using the error handlers
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		// connectDB
+		app.listen(port, () => console.log(`Server listening on PORT ${port}...`));
+	} catch (error) {
+		console.log(error); 
+	}
+};
+
+start();
---
+++ src/middleware/error-handler.js
@@ -0,0 +1,8 @@
+const errorHandlerMiddleware = async (err, req, res, next) => {
+	console.log(err);
+	return res
+		.status(500)
+		.json({ msg: "Something is not right, please try again" });
+};
+
+module.exports = errorHandlerMiddleware;
