184da5976dfc1eebe307480968658093bfd92702
Moyinoluwalogo <m.oladipsgmail.com>
2022-09-30 21:39:10 +0100

    Added a page functionality to the search function, formatting how the results would be viewed when sent, this was done using the liit and the skip functionality together. Also commented the code where needed

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,7 +1,11 @@
 const productList = require("../models/productModel");
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await productList.find({}).select("name price rating");
+	const products = await productList.find({})
+	.sort('price')
+	.select("name price rating")
+	.limit(10)
+	.skip(10);
 	res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -21,23 +25,31 @@ const getAllProducts = async (req, res) => {
 		queryObject.name = { $regex: name, $options: "i" }; //Regex is used here for pattern matching to search for like the first two letters and return all matching results. 'i' is the option indicating that the case of the lettrs dont matter as long as the pattern match
 	}
 	// console.log(queryObject);
-	let searchResult = productList.find(queryObject); //filter to be used can be selected by inserting the category into the find
+
+	// Sorting Functionality for the search filter
+	let searchResult = productList.find(queryObject); //filters to be used for sorting can be selected by inserting the category into the find
 	if (sort) {
 		const sortParams = sort.split(",").join(" "); //in the event where a number of variables are to be used for the sorting, this formats them to the proper syntax
-		searchResult = searchResult.sort(sortParams)
+		searchResult = searchResult.sort(sortParams);
 	} else {
-		searchResult = searchResult.sort('createdAt')
+		searchResult = searchResult.sort("createdAt");
 	}
 
-	// Functionality to selct which fields to be dispalyed in the results
+	// Functionality to select which fields to be dispalyed in the search results
 	if (fields) {
 		const selectFields = fields.split(",").join(" ");
-		searchResult = searchResult.select(selectFields)
-
+		searchResult = searchResult.select(selectFields);
 	}
 
+	//Limit functionality to control the amount of products returned as results used in conjunction with skip which skips a certain number of product to achieve pagination
+	const page = Number(req.query.page) || 1
+	const limit = Number(req.query.limit) || 10
+	const skip = (page -1) * limit
+
+	searchResult = searchResult.skip(skip).limit(limit)
+
 	const products = await searchResult;
-	res.status(200).json({ products, nbHits: products.length }); //nbhits shows the number of results that fulfill the search criteria
+	res.status(200).json({ nbHits: products.length, products }); //nbhits shows the number of results that fulfill the search criteria
 };
 
 const addProduct = async (req, res) => {
